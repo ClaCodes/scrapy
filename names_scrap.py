c14e45a4570bdad424a7e7759476d451f41a9ea5
rfs <remo@mountain-yard.ch>
2023-07-15 12:32:55 +0200

    improve breadth_first

--- names_scrap.py
+++ names_scrap.py
@@ -36,44 +36,42 @@ def depth_first(query):
             depth_first(query + ' ')
 
 
-def breadth_first(query, max_depth):
+def breadth_first(query, min_queue_size):
     query_queue = [c for c in string.ascii_lowercase]
+    while len(query_queue) > 0:
+       next_query = query_queue.pop(0)
+       n_list = look_up(next_query)["suggestions"]
+       print(f"next_query={next_query}, len(query_queue)={len(query_queue)}")
+       store_names(n_list)
+       if len(query_queue) < min_queue_size and len(n_list) > 0:
+           for c in string.ascii_lowercase:
+               query_queue.append(next_query + c)
+               query_queue.append(next_query + " ")
+
+
+if __name__ == "__main__":
+    max = 14090
+    # TODO why?
+    max *= 2
+
+
     try:
-        while True:
-            next_query = query_queue.pop(0)
-            n_list = look_up(next_query)["suggestions"]
-            print(f"next_query={next_query}, len(query_queue)={len(query_queue)}")
-            store_names(n_list)
-            if len(n_list) >= 15 and len(next_query) < max_depth:
-                for c in string.ascii_lowercase:
-                    query_queue.append(next_query + c)
-                query_queue.append(next_query + " ")
-    except IndexError:
-        return
+        with open("names.json", "r") as f:
+            s_names = json.load(f)
+    except FileNotFoundError:
+        s_names = [None for _ in range(0, max)]
 
+    try:
+        with open("request_cache.json", "r") as f:
+            requests_cache = json.load(f)
+    except FileNotFoundError:
+        requests_cache = {}
 
-max = 14090
-# TODO why?
-max *= 2
-
-
-try:
-    with open("names.json", "r") as f:
-        s_names = json.load(f)
-except FileNotFoundError:
-    s_names = [None for _ in range(0, max)]
-
-try:
-    with open("request_cache.json", "r") as f:
-        requests_cache = json.load(f)
-except FileNotFoundError:
-    requests_cache = {}
-
-try:
-    # depth_first("")
-    breadth_first("", 3)
-finally:
-    with open("request_cache.json", "w") as f:
-        json.dump(requests_cache, f, indent=4)
-    with open("names.json", "w") as f:
-        json.dump(s_names, f, indent=4)
+    try:
+        # depth_first("")
+        breadth_first("", 50)
+    finally:
+        with open("request_cache.json", "w") as f:
+            json.dump(requests_cache, f, indent=4)
+        with open("names.json", "w") as f:
+            json.dump(s_names, f, indent=4)
