5fa93096023774408810e3c0daa6b98f6c662d13
rfs <remo@mountain-yard.ch>
2023-07-20 17:10:25 +0200

    feat: upload extracted schwinger to database

--- etl/src/etl-runner.js
+++ etl/src/etl-runner.js
@@ -1,14 +1,21 @@
 import {extractSchwinger, fetchSchwinger} from './extract.js';
 import {transformSchwinger} from "./transform.js";
-import {loadSchwingerToFile} from "./load.js";
+import {loadSchwingerToDatabase, loadSchwingerToFile} from "./load.js";
 import {createSearch} from "./search.js";
+import {database} from "./database.js";
+
+
+/** @type {LoadConfig} */
+const loadConfig = {
+    path: `./dist/data/${new Date().toISOString()}_schwinger.json`,
+}
 
 await extractSchwinger(
     createSearch('a', 'zz'),
     fetchSchwinger,
     transformSchwinger,
     loadSchwingerToFile,
-    {
-        path: `./dist/data/${new Date().toISOString()}_schwinger.json`,
-    }
+    loadConfig,
 );
+
+await loadSchwingerToDatabase(database, loadConfig);
