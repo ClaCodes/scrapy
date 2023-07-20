4b658cd50fbd755749c661de7878c6b1b82f2602
rfs <remo@mountain-yard.ch>
2023-07-20 15:56:52 +0200

    refactor: remove unnecessary comments

--- etl/src/database.js
+++ etl/src/database.js
@@ -1,10 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBUkI5ks9lEFuqejngEBD4NY-DDRAWeef0",
     authDomain: "scrapy-1281d.firebaseapp.com",
@@ -15,7 +11,6 @@ const firebaseConfig = {
     measurementId: "G-MX1MMW28KP"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const database = getFirestore(app);
+export const database = getFirestore(app);
