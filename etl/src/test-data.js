7fb69e9e0f242e1a54bac03b1192e05be7c5870c
rfs <remo@mountain-yard.ch>
2023-07-21 23:37:02 +0200

    feat: add test data to facilitate implementation of update resp. diffing functionality

--- etl/src/test-data.js
+++ etl/src/test-data.js
@@ -97,3 +97,521 @@ export const response_for_abderhalden_maurice = {
     "suggestions": []
 };
 
+export const existingSchwinger = [
+    {
+        "id": 4774,
+        "firstName": "Stefan",
+        "lastName": "Haab"
+    },
+    {
+        "id": 5175,
+        "firstName": "Isaac",
+        "lastName": "Henchoz"
+    },
+    {
+        "id": 10225,
+        "firstName": "Mahla",
+        "lastName": "Saadat"
+    },
+    {
+        "id": 10305,
+        "firstName": "Andri",
+        "lastName": "Schaad"
+    },
+    {
+        "id": 10306,
+        "firstName": "Joëlle",
+        "lastName": "Schaad"
+    },
+    {
+        "id": 10307,
+        "firstName": "Philipp",
+        "lastName": "Schaad"
+    },
+    {
+        "id": 10308,
+        "firstName": "Remo",
+        "lastName": "Schaad"
+    },
+    {
+        "id": 10309,
+        "firstName": "Silvan",
+        "lastName": "Schaad"
+    },
+    {
+        "id": 9357,
+        "firstName": "Bernhard",
+        "lastName": "Raaflaub"
+    },
+    {
+        "id": 4775,
+        "firstName": "Elias",
+        "lastName": "Haag"
+    },
+    {
+        "id": 4776,
+        "firstName": "Fabio",
+        "lastName": "Haag"
+    },
+    {
+        "id": 4777,
+        "firstName": "Roman",
+        "lastName": "Haag"
+    },
+    {
+        "id": 4778,
+        "firstName": "Yanik",
+        "lastName": "Haag"
+    },
+    {
+        "id": 7044,
+        "firstName": "Andreas",
+        "lastName": "Laager"
+    },
+    {
+        "id": 7045,
+        "firstName": "Kaspar",
+        "lastName": "Laager"
+    },
+    {
+        "id": 7046,
+        "firstName": "Michael",
+        "lastName": "Laager"
+    },
+    {
+        "id": 11989,
+        "firstName": "Aamir",
+        "lastName": "Stutzer"
+    },
+    {
+        "id": 5232,
+        "firstName": "Daan",
+        "lastName": "Hermann"
+    },
+    {
+        "id": 898,
+        "firstName": "Aaron",
+        "lastName": "Beer"
+    },
+    {
+        "id": 1276,
+        "firstName": "Aaron",
+        "lastName": "Birchmeier"
+    },
+    {
+        "id": 1869,
+        "firstName": "Aaron",
+        "lastName": "Bucher"
+    },
+    {
+        "id": 2461,
+        "firstName": "Aaron",
+        "lastName": "Colldeweih"
+    },
+    {
+        "id": 2905,
+        "firstName": "Aaron",
+        "lastName": "Duss"
+    },
+    {
+        "id": 3000,
+        "firstName": "Aaron",
+        "lastName": "Egli"
+    },
+    {
+        "id": 3388,
+        "firstName": "Aaron",
+        "lastName": "Fässler"
+    },
+    {
+        "id": 3768,
+        "firstName": "Aaron",
+        "lastName": "Frey"
+    },
+    {
+        "id": 3911,
+        "firstName": "Aaron",
+        "lastName": "Gähwiler"
+    },
+    {
+        "id": 4087,
+        "firstName": "Aaron",
+        "lastName": "Gempeler"
+    },
+    {
+        "id": 4288,
+        "firstName": "Aaron",
+        "lastName": "Gisler"
+    },
+    {
+        "id": 4662,
+        "firstName": "Aaron",
+        "lastName": "Gübeli"
+    },
+    {
+        "id": 4699,
+        "firstName": "Aaron",
+        "lastName": "Guntersweiler"
+    },
+    {
+        "id": 5252,
+        "firstName": "Aaron",
+        "lastName": "Hersberger"
+    },
+    {
+        "id": 5440,
+        "firstName": "Aaron",
+        "lastName": "Hohler"
+    },
+    {
+        "id": 4779,
+        "firstName": "Andreas",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4780,
+        "firstName": "Dario",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4781,
+        "firstName": "Fabian",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4782,
+        "firstName": "Hendrik",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4783,
+        "firstName": "Julian",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4784,
+        "firstName": "Kevin",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4785,
+        "firstName": "Laurin",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4786,
+        "firstName": "Louis",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4787,
+        "firstName": "Marc",
+        "lastName": "Haas"
+    },
+    {
+        "id": 4788,
+        "firstName": "Matthias",
+        "lastName": "Haas"
+    },
+    {
+        "id": 6523,
+        "firstName": "Laurin",
+        "lastName": "Klaas"
+    },
+    {
+        "id": 10116,
+        "firstName": "Niclaas",
+        "lastName": "Rungger"
+    },
+    {
+        "id": 3952,
+        "firstName": "Narantulga",
+        "lastName": "Ganbaatar"
+    },
+    {
+        "id": 1,
+        "firstName": "Fabio",
+        "lastName": "Abächerli"
+    },
+    {
+        "id": 2,
+        "firstName": "Lars",
+        "lastName": "Abächerli"
+    },
+    {
+        "id": 3,
+        "firstName": "Marco",
+        "lastName": "Abächerli"
+    },
+    {
+        "id": 4,
+        "firstName": "Jasin",
+        "lastName": "Abaya"
+    },
+    {
+        "id": 2618,
+        "firstName": "Alexandre",
+        "lastName": "Delabays"
+    },
+    {
+        "id": 2619,
+        "firstName": "Julien",
+        "lastName": "Delabays"
+    },
+    {
+        "id": 2620,
+        "firstName": "Thibaud",
+        "lastName": "Delabays"
+    },
+    {
+        "id": 3883,
+        "firstName": "Björn",
+        "lastName": "Gabathuler"
+    },
+    {
+        "id": 3884,
+        "firstName": "Nevio",
+        "lastName": "Gabathuler"
+    },
+    {
+        "id": 3885,
+        "firstName": "Rino",
+        "lastName": "Gabathuler"
+    },
+    {
+        "id": 5898,
+        "firstName": "Shaban",
+        "lastName": "Isufi"
+    },
+    {
+        "id": 7709,
+        "firstName": "Lamine",
+        "lastName": "Mamadou"
+    },
+    {
+        "id": 10226,
+        "firstName": "Fabio",
+        "lastName": "Sabatino"
+    },
+    {
+        "id": 5,
+        "firstName": "Janik",
+        "lastName": "Abbühl"
+    },
+    {
+        "id": 6,
+        "firstName": "Jonas",
+        "lastName": "Abbühl"
+    },
+    {
+        "id": 7,
+        "firstName": "Levin",
+        "lastName": "Abbühl"
+    },
+    {
+        "id": 8,
+        "firstName": "Marc",
+        "lastName": "Abbühl"
+    },
+    {
+        "id": 9,
+        "firstName": "Mike",
+        "lastName": "Abbühl"
+    },
+    {
+        "id": 10,
+        "firstName": "Adrian",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 11,
+        "firstName": "Beat",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 12,
+        "firstName": "Deon",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 13,
+        "firstName": "Jörg",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 14,
+        "firstName": "Lukas",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 15,
+        "firstName": "Maurice",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 16,
+        "firstName": "Sammy",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 17,
+        "firstName": "Urs",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 18,
+        "firstName": "Yannick",
+        "lastName": "Abderhalden"
+    },
+    {
+        "id": 8504,
+        "firstName": "Abdalmedzit",
+        "lastName": "Muslierski"
+    },
+    {
+        "id": 8505,
+        "firstName": "Abdul",
+        "lastName": "Muslievski"
+    },
+    {
+        "id": 12132,
+        "firstName": "Abdourahaman",
+        "lastName": "Tchedre"
+    },
+    {
+        "id": 19,
+        "firstName": "Robin",
+        "lastName": "Abegg"
+    },
+    {
+        "id": 20,
+        "firstName": "Silvan",
+        "lastName": "Abegg"
+    },
+    {
+        "id": 21,
+        "firstName": "Markus",
+        "lastName": "Abegglen"
+    },
+    {
+        "id": 22,
+        "firstName": "Linus",
+        "lastName": "Aberle"
+    },
+    {
+        "id": 3013,
+        "firstName": "Isabel",
+        "lastName": "Egli"
+    },
+    {
+        "id": 3437,
+        "firstName": "Rolf",
+        "lastName": "Feierabend"
+    },
+    {
+        "id": 3886,
+        "firstName": "Karim",
+        "lastName": "Gaber"
+    },
+    {
+        "id": 4256,
+        "firstName": "Gervais",
+        "lastName": "Gilabert"
+    },
+    {
+        "id": 4479,
+        "firstName": "Alfred",
+        "lastName": "Graber"
+    },
+    {
+        "id": 4480,
+        "firstName": "Andreas",
+        "lastName": "Graber"
+    },
+    {
+        "id": 4481,
+        "firstName": "Bänz",
+        "lastName": "Graber"
+    },
+    {
+        "id": 4482,
+        "firstName": "Bruno",
+        "lastName": "Graber"
+    },
+    {
+        "id": 4483,
+        "firstName": "Colin",
+        "lastName": "Graber"
+    },
+    {
+        "id": 4484,
+        "firstName": "Daniel",
+        "lastName": "Graber"
+    },
+    {
+        "id": 4485,
+        "firstName": "Felix",
+        "lastName": "Graber"
+    },
+    {
+        "id": 23,
+        "firstName": "Peter",
+        "lastName": "Abgospohn"
+    },
+    {
+        "id": 24,
+        "firstName": "Mattia",
+        "lastName": "Abgottspon"
+    },
+    {
+        "id": 32,
+        "firstName": "Fabio",
+        "lastName": "Accordino"
+    },
+    {
+        "id": 93,
+        "firstName": "Fabian",
+        "lastName": "Aebersold"
+    },
+    {
+        "id": 148,
+        "firstName": "Fabian",
+        "lastName": "Aeschlimann"
+    },
+    {
+        "id": 274,
+        "firstName": "Fabian",
+        "lastName": "Amrein"
+    },
+    {
+        "id": 315,
+        "firstName": "Fabian",
+        "lastName": "Anderegg"
+    },
+    {
+        "id": 390,
+        "firstName": "Fabian",
+        "lastName": "Aregger"
+    },
+    {
+        "id": 426,
+        "firstName": "Fabian",
+        "lastName": "Arnold"
+    },
+    {
+        "id": 427,
+        "firstName": "Fabio",
+        "lastName": "Arnold"
+    },
+    {
+        "id": 551,
+        "firstName": "Fabian",
+        "lastName": "Bachmann"
+    },
+    {
+        "id": 593,
+        "firstName": "Fabian",
+        "lastName": "Bader"
+    },
+];
+
