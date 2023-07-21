c0e14c7c1ff369ba8bc648162eea75ae04c6da52
rfs <remo@mountain-yard.ch>
2023-07-21 23:37:44 +0200

    refactor: manually upload extracted data

--- etl/src/upload.js
+++ etl/src/upload.js
@@ -3,7 +3,7 @@ import {database} from "./database.js";
 
 /** @type {LoadConfig} */
 const loadConfig = {
-    path: './dist/data/2023-07-20T13:17:37.244Z_schwinger.json',
+    path: './dist/data/2023-07-21T17:00:18.035Z_schwinger.json',
 }
 
 await loadSchwingerToDatabase(database, loadConfig);
