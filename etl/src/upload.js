0d8cc43112a496876e5e42c20ccdb3c57c2ae0ff
rfs <remo@mountain-yard.ch>
2023-07-24 19:19:53 +0200

    refactor: remove superfluous param

--- etl/src/upload.js
+++ etl/src/upload.js
@@ -1,10 +1,9 @@
 import {storeSchwingerToDatabase} from "./load.js";
-import {database} from "./database.js";
 
 /** @type {LoadConfig} */
 const loadConfig = {
     path: './dist/data/2023-07-21T17:00:18.035Z_schwinger.json',
 }
 
-await storeSchwingerToDatabase(database, loadConfig);
+await storeSchwingerToDatabase(loadConfig);
 
